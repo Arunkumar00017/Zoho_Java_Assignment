cd98539e9e58bd6c291f88f338014d466604cf58
Arunkumar00017 <127184137+Arunkumar00017@users.noreply.github.com>
2025-04-08 18:48:28 +0530

    Delete oops/Assignment3/oopsAss3ques5.java

--- oops/Assignment3/oopsAss3ques5.java
+++
@@ -1,5 +0,0 @@
-public class oopsAss3ques5 {
-    public static void main(String[] args) {
-        System.out.println("Visit https://exercism.org/tracks/java/exercises/wizards-and-warriors to complete this exercise.");
-    }
-}
